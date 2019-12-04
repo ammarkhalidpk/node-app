67d17b2cb5edbf61c504538460007cec47ae2deb
Ammar Khalid <ammarkhalid1@gmail.com>
2019-12-04 13:06:05 +1100

    CI/CD POC

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../index.js');
+describe('GET /', function() {
+ it('Respond with Hello World!', function(done) {
+ //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('Welcome to CI/CD POC', done);
+ });
+});
