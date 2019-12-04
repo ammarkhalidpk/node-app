863c7db1061a1be74a66eb1a94ebe8d14c8ce48f
Ammar Khalid <ammarkhalid1@gmail.com>
2019-12-04 18:33:17 +1100

    Adding Code Smell

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ var app = express();
 
 debugger;
 
-var falseCompare  = 1;
-if (falseCompare == '1'){
-    console.log('invalid way of comparison');
+var falseCompare = 1;
+try{
+    if (falseCompare == '1'){
+        console.log('invalid way of comparison');
+    }
+}catch(ex){
+    throw ex;
 }
+
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
  res.send('Welcome to CI/CD POC');
